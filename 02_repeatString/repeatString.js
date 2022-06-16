516a3016318cc5cbe647e4339e84cf72e3b33f88
kmckelvey2020 <103072346+kmckelvey2020@users.noreply.github.com>
2022-06-16 09:55:40 -0400

    Completed javascript exercises 1-4.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let message='';
+    if(typeof num==="number" && num>=0) {
+        for(let i=0; i<num; i++) {
+            message += `${str}`;
+        }
+        return message;
+    } else return 'ERROR';
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originalArr, ...targets) {
+    if(Array.isArray(originalArr)) {
+        const resultArr = originalArr.filter((element) => !targets.includes(element));
+        return resultArr;
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line
